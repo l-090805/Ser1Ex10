2125e20843b381687bf1f334353852a9f50faab3
falau <falau@LENOVO>
2024-10-31 19:49:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Ser1Ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti numarul n:");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n <= 1)
+            {
+                Console.WriteLine("Numerele prime nu pot fi mai mici sau egale cu 1");
+            }
+            else if (n == 2)
+            {
+                Console.WriteLine("Numarul este prim");
+            }
+            else
+            {
+                bool estePrim = true;
+
+                for (int i = 2; i <= Math.Sqrt(n); i++)
+                {
+
+                    if (n % i == 0)
+                    {
+                        estePrim = false;
+                        Console.WriteLine("Numarul nu este prim");
+                        break;
+                    }
+                }
+
+                if(estePrim)
+                {
+                    Console.WriteLine("Numarul este prim");
+                }
+            }                    
+        }
+    }
+}
